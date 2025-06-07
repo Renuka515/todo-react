12f2c3ab4f085e770abc477a445ffac858036c5b
Renuka515 <renukakr2022@gmail.com>
2025-06-07 15:00:10 +0530

    intialcommit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+// !! FEATURES TO BE IMPLEMENTED
+// If a todo is added clear the input field
+// Mark as completed functionality
+// if the todo is empty do not submit
+import { useState } from "react";
+
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleTodos = () => {
+    setTodos((prev) => [...prev, todo]);
+  };
+
+  return (
+    <>
+      <div>
+        <main className="container " style={{ marginTop: 40 }}>
+          <header>
+            <h1>Todo App</h1>
+          </header>
+
+          <section>
+            <h2>Add New Todo</h2>
+            <div className="grid">
+              <input
+                onChange={(e) => setTodo(e.target.value)}
+                type="text"
+                placeholder="Enter your todo..."
+                aria-label="Todo input"
+              />
+              <button onClick={handleTodos}>Add Todo</button>
+            </div>
+          </section>
+
+          <section>
+            <h2>Added Todos</h2>
+            <ul>
+              {todos.map((todo, key) => (
+                <li>
+                  <div className="grid">
+                    <div>
+                      <strong>
+                        {key + 1}. {todo}
+                      </strong>
+                    </div>
+                    <div>
+                      <button className="secondary outline">Completed</button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default App;
